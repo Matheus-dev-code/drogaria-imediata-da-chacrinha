65417e26c55bcbf86b2a967b4d8c31515b3f2fe8
Matheus-dev-code <matheusrodrigues7728@gmail.com>
2026-09-10 20:28:34 -0300

    Add files via upload

---
+++ js/data/produtos/diana.js
@@ -0,0 +1,58 @@
+// ========================================
+// PRODUTOS DIANA - ABSORVENTES
+// ========================================
+
+const produtosDiana = [
+
+    // ========== ABSORVENTES DIANA (4) ==========
+
+    {
+        id: 1701,
+        nome: "Absorvente Diana Noturno com Abas Suave 28 Unidades",
+        marca: "Diana",
+        tipo: "Higiene",
+        categoria: "Higiene Pessoal",
+        descricao: "Absorvente Noturno com Abas Cobertura Suave 28 unidades",
+        preco: "R$ 19,99 ",
+        imagem: "img/diana/noturno/28un.jpe",
+        destaque: false,
+        esgotado: false
+    },
+    {
+        id: 1702,
+        nome: "Absorvente Diana Noturno com Abas Suave 8 Unidades",
+        marca: "Diana",
+        tipo: "Higiene",
+        categoria: "Higiene Pessoal",
+        descricao: "Absorvente Noturno com Abas Cobertura Suave 8 unidades",
+        preco: "R$ 6,99 ",
+        imagem: "img/diana/noturno/8un.jpe",
+        destaque: false,
+        esgotado: false
+    },
+    {
+        id: 1703,
+        nome: "Absorvente Diana com Abas Suave 16 Unidades",
+        marca: "Diana",
+        tipo: "Higiene",
+        categoria: "Higiene Pessoal",
+        descricao: "Absorvente com Abas Cobertura Suave 16 unidades",
+        preco: "R$ 7,49 ",
+        imagem: "img/diana/comum/16un.jpe",
+        destaque: false,
+        esgotado: false
+    },
+    {
+        id: 1704,
+        nome: "Absorvente Diana com Abas Suave 8 Unidades",
+        marca: "Diana",
+        tipo: "Higiene",
+        categoria: "Higiene Pessoal",
+        descricao: "Absorvente com Abas Cobertura Suave 8 unidades",
+        preco: "R$ 4,99 ",
+        imagem: "img/diana/comum/8un.jpe",
+        destaque: false,
+        esgotado: false
+    }
+
+];
