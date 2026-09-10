2cd84a3fd0c6d930e36b50b07f6e857276f8cce1
Matheus-dev-code <matheusrodrigues7728@gmail.com>
2026-09-10 20:57:49 -0300

    Add files via upload

---
+++ js/data/produtos/always.js
@@ -0,0 +1,82 @@
+// ========================================
+// PRODUTOS ALWAYS - ABSORVENTES NOTURNOS
+// ========================================
+
+const produtosAlways = [
+
+    // ========== ABSORVENTES ALWAYS NOTURNOS (6) ==========
+
+    {
+        id: 1806,
+        nome: "Absorvente Always Noturno Tamanho XXG com Abas Suave 10 Unidades",
+        marca: "Always",
+        tipo: "Higiene",
+        categoria: "Higiene Pessoal",
+        descricao: "Absorvente Noturno Tamanho XXG com Abas Cobertura Suave 10 unidades",
+        preco: "R$ 29,99 ",
+        imagem: "img/always/xxg-10.jpe",
+        destaque: false,
+        esgotado: false
+    },
+    {
+        id: 1801,
+        nome: "Absorvente Always Noturno Tamanho G com Abas Suave 32 Unidades",
+        marca: "Always",
+        tipo: "Higiene",
+        categoria: "Higiene Pessoal",
+        descricao: "Absorvente Noturno Tamanho G com Abas Cobertura Suave 32 unidades",
+        preco: "R$ 34,99 ",
+        imagem: "img/always/suave-32.jpe",
+        destaque: false,
+        esgotado: false
+    },
+    {
+        id: 1802,
+        nome: "Absorvente Always Noturno Tamanho G com Abas Suave 16 Unidades",
+        marca: "Always",
+        tipo: "Higiene",
+        categoria: "Higiene Pessoal",
+        descricao: "Absorvente Noturno Tamanho G com Abas Cobertura Suave 16 unidades",
+        preco: "R$ 18,49 ",
+        imagem: "img/always/suave-16un.jpe",
+        destaque: false,
+        esgotado: false
+    },
+    {
+        id: 1803,
+        nome: "Absorvente Always Noturno Tamanho G com Abas Seco 16 Unidades",
+        marca: "Always",
+        tipo: "Higiene",
+        categoria: "Higiene Pessoal",
+        descricao: "Absorvente Noturno Tamanho G com Abas Cobertura Seco 16 unidades",
+        preco: "R$ 18,49 ",
+        imagem: "img/always/seco-16un.jpe",
+        destaque: false,
+        esgotado: false
+    },
+    {
+        id: 1804,
+        nome: "Absorvente Always Noturno Tamanho G com Abas Suave 8 Unidades",
+        marca: "Always",
+        tipo: "Higiene",
+        categoria: "Higiene Pessoal",
+        descricao: "Absorvente Noturno Tamanho G com Abas Cobertura Suave 8 unidades",
+        preco: "R$ 11,49 ",
+        imagem: "img/always/suave-10un.jpe",
+        destaque: false,
+        esgotado: false
+    },
+    {
+        id: 1805,
+        nome: "Absorvente Always Noturno Tamanho G com Abas Seco 8 Unidades",
+        marca: "Always",
+        tipo: "Higiene",
+        categoria: "Higiene Pessoal",
+        descricao: "Absorvente Noturno Tamanho G com Abas Seco 8 unidades",
+        preco: "R$ 10,99 ",
+        imagem: "img/always/seco-8un.jpe",
+        destaque: false,
+        esgotado: false
+    }
+
+];
